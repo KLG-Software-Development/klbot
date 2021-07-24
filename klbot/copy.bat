8d14b274bf8d8cf9d974c80561afbfb44b6aef06
Fireplace <37326218+Deleeete@users.noreply.github.com>
2021-07-24 14:08:03 +0800

    Delete copy.bat

--- klbot/copy.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-copy /Y "C:\A\Programming\Projects\klbot\klbot\bin\Publish\net5.0\klbot" "C:/inetpub/wwwroot/"
-copy /Y "C:\A\Programming\Projects\klbot\klbot\fuckpat" "C:/inetpub/wwwroot/"
