2444fe8ba4e1c93e728df27232474b6746b23276
Edistein <365802825@qq.com>
2021-07-28 20:00:39 +0800

    拓宽了上号模块的反应情景，并且输出从“上号”改成了复读

--- klbotlib/src/modules/上号Module.cs
+++ klbotlib/src/modules/上号Module.cs
@@ -1,19 +1,31 @@
 ﻿namespace klbotlib.Modules
 {
+    /// <summary>
+    /// 上号模块
+    /// </summary>
     public class 上号Module : SingleTypeModule<MessagePlain>
     {
         [ModuleStatus]
         private string LastMsg = "";
+        private bool Is上号(string text) => text.Length <= 5 && text.Contains("上号");
+        /// <summary>
+        /// 关闭模块签名
+        /// </summary>
         public override bool UseSignature => false;
+        /// <summary>
+        /// 过滤器：处理任何消息
+        /// </summary>
         public override bool Filter(MessagePlain msg) => true;
+        /// <summary>
+        /// 处理器：内容包含上号且不长于五个字符，则复读内容；
+        /// 另外，缓存当前消息到LastMsg中，用于下一次判断是否是同一轮上号消息。如果是同一轮则不回复。
+        /// </summary>
         public override string Processor(MessagePlain msg)
         {
-            string output;
-            if (msg.Text.Trim() == "上号" && LastMsg != "上号")
-                output = "上号";
-            else
-                output = string.Empty;
-            LastMsg = msg.Text.Trim();
+            string msg_text = msg.Text.Trim(), output = string.Empty;
+            if (Is上号(msg_text) && !Is上号(LastMsg))
+                output = msg_text;
+            LastMsg = msg_text;
             return output;
         }
     }
