916dbc6c1ad526d5d04ac7ccdec2aa5ae98de38a
Edistein <365802825@qq.com>
2022-04-19 05:07:26 +0100

    EchoModule实现基本逻辑框架

---
+++ ModuleCollection/EchoModule.cs
@@ -0,0 +1,24 @@
+﻿using klbotlib;
+using klbotlib.Modules;
+
+namespace ModuleCollection;
+
+/// 消息回显模块
+public class EchoModule : Module
+{
+    /// <inheritdoc/>
+    public override string FriendlyName => "回显模块";
+    /// <inheritdoc/>
+    public override bool UseSignature => false;
+
+    /// <inheritdoc/>
+    public override string Filter(Message msg)
+    {
+        return "in";
+    }
+    /// <inheritdoc/>
+    public override string Processor(Message msg, string filterOut)
+    {
+        return msg.ToString();
+    }
+}
