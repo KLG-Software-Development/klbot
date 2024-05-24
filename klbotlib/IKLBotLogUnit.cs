2f163228f73293da57b884383d562e9a5f6efc12
Edistein <365802825@qq.com>
2024-05-25 04:43:57 +0800

    日志机制逐步引入

---
+++ klbotlib/IKLBotLogUnit.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace klbotlib;
+
+/// <summary>
+/// KLBot Log Unit
+/// </summary>
+public interface IKLBotLogUnit
+{
+    /// <summary>
+    /// Name of log unit
+    /// </summary>
+    string LogUnitName { get; }
+}
+
+internal static class IKLBotLogUnitExtension
+{
+    [Conditional("DEBUG")]
+    public static void DebugLog(this IKLBotLogUnit component, string s)
+        => Trace.WriteLine($"[{component.LogUnitName}] {s}");
+    public static void Log(this IKLBotLogUnit component, string s)
+        => Trace.WriteLine($"[{component.LogUnitName}] {s}");
+}
